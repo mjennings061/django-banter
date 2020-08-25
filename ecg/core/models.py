78e25adfae13e556b0aab4f95d8989b562d85051
Michael Jennings <mjennings061@gmail.com>
2020-08-25 14:38:38 +0100

    Change project to include user login and navbar - Start from scratch

---
+++ ecg/core/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+from django.contrib.auth.models import User     # import django's model for the user
+
+# Create your models here.
---
+++ ecg/venv/Lib/site-packages/filer/admin/thumbnailoptionadmin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from django.contrib import admin
+
+
+class ThumbnailOptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'width', 'height')
