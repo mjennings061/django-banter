1e820a67cf61445b67f9da711923ea4108cf8fb1
Michael Jennings <mjennings061@gmail.com>
2020-08-26 09:36:31 +0100

    Update admin site with File

--- ecg/core/admin.py
+++ ecg/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from .models import File
 
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['user, id, name, uploaded_file']
+
+
+admin.site.register(File)
--- ecg/core/models.py
+++ ecg/core/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User     # import django's model for the user
-import uuid
+import uuid     # used for unique ID generation
 import os   # used for filename changes
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 
 
 # Uploaded file model (connects to UploadedFileForm)
 class File(models.Model):
-    def content_filename(instance, filename):   # dynamic filename changing for the uploaded file
-        ext = filename.split('.')[-1]
-        filename = "%s_%s.%s" % (instance.user, instance.id, ext)
-        return os.path.join(settings.MEDIA_ROOT, filename)
+    def content_filename(instance, filename):   # dynamic filename changing for the uploaded file when saving
+        ext = filename.split('.')[-1]   # get file extension
+        filename = "%s_%s.%s" % (instance.user, instance.id, ext)   # filename is username_id.ext e.g mj_45ds.jpeg
+        return os.path.join(settings.MEDIA_ROOT, filename)  # return filepath for storage
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, default='1')
-    uploaded_file = models.FileField(upload_to=content_filename, null=True, max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    # link to the user uploading the file
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # unique ID for the file
+    name = models.CharField(max_length=100, default='1')    # user-given name for the file
+    uploaded_file = models.FileField(upload_to=content_filename, null=True, max_length=200)     # the file itself
 
     def __str__(self):
-        return self.id
+        return f"{self.id}"
