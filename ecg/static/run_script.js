b6ac5d7fc608b271c28c80fe36de9a7f422d0c9b
Michael Jennings <mjennings061@gmail.com>
2020-10-16 12:41:03 +0100

    Re-arranged javascript into separate files

---
+++ ecg/static/run_script.js
@@ -0,0 +1,11 @@
+$(function(){
+    function hideInputs() {
+        $("#id_script").destroy();
+    }
+
+    $("#id_data_input").on('change', function(){
+        // filter script options based on data_input.format
+    });
+
+    hideInputs();
+});
