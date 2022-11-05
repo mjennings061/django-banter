bc6930f99f3470e69e8c4912c0758c3885bea882
Michael Jennings <michael.jennings@b-secur.com>
2022-11-05 12:06:46 +0000

    Removed ignored files.

--- ecg/media/algorithm/handler.m
+++
@@ -1,8 +0,0 @@
-function y = handler(script_file_path, data_file_path)
-    fclose('all');
-    % Sanitise both args (regex)
-    addpath('C:\Users\MJ\OneDrive - Ulster University\Documents\PhD\Django\django-banter\ecg\media\algorithm');
-    [script_path,script_name,script_ext] = fileparts(script_file_path);
-    func_to_exec = str2func(script_name);
-    y = func_to_exec(data_file_path);
-end
